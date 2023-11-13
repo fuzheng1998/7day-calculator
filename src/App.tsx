fe409003291e449c0dbd68b8930ad09bd93ee541
Zheng Fu <24203166+fuzheng1998@users.noreply.github.com>
2023-11-13 21:01:53 +1100

    Add decimal point and operator validation logic to calculator app

--- src/App.tsx
+++ src/App.tsx
@@ -2,45 +2,67 @@
  * Renders a calculator app component.
  * @returns {JSX.Element} Calculator app component.
  */
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
+import { log } from "console";
 
 function App() {
   const [input, setInput] = useState<string>("");
+  const [lastPressedWasCalculation, setLastPressedWasCalculation] =
+    useState(false);
 
   const handleButtonClick = (value: string) => {
-    // if value is number or decimal, append to input, decimal can only be used once
-    if (value === "." && input.includes(".")) {
-      alert("Decimal can only be used once");
+    const operators = ["+", "-", "*", "/"];
+    const isDigit = /^[0-9]$/.test(value);
+    // Check if the last character is an operator
+    const lastCharIsOperator = operators.includes(input.slice(-1));
+
+    // Check for consecutive operators and prevent them
+    if (operators.includes(value) && (input === "" || lastCharIsOperator)) {
+      alert(
+        "Operator cannot be used as first input and cannot be used consecutively"
+      );
       return;
     }
-    setInput(input + value);
-    // if value is percentage, divide by 100
-    if (value === "%") {
-      setInput((parseFloat(input) / 100).toString());
-    }
-    // if value is x^2, square the number
-    if (value === "x^2") {
-      setInput((parseFloat(input) * parseFloat(input)).toString());
-    }
-    // if value is +-, change the sign of the number
-    if (value === "+-") {
-      setInput((parseFloat(input) * -1).toString());
-    }
-    // if value is sqrt, square root the number
-    if (value === "sqrt") {
-      setInput(Math.sqrt(parseFloat(input)).toString());
-    }
-    // if value is 1/x, divide 1 by the number, 0 is not allowed
-    if (value === "1/x") {
-      if (parseFloat(input) === 0) {
-        // alert user that 0 is not allowed
-        alert("0 is not allowed");
-        // clear the input
-        setInput("");
-      } else {
-        setInput((1 / parseFloat(input)).toString());
+    
+
+    // Handling decimal point logic
+    if (value === ".") {
+      const parts = input.split(/[\+\-\*\/]/); // Split input by operators
+      const lastNumber = parts.pop(); // Get the last number entered
+
+      if (lastNumber && lastNumber.includes(".")) {
+        alert("Decimal can only be used once per number");
+        return;
       }
+    }
+
+    // Append the value to the input
+    setInput(input + value);
+
+    // percentage operations
+    if (value === "%") {
+      try {
+        const result = eval(input);
+        const percentageResult = result / 100;
+        setInput(percentageResult.toString());
+        setLastPressedWasCalculation(true);
+      } catch (error) {
+        setInput("Error");
+        setLastPressedWasCalculation(false);
+      }
+      return;
+    }
+    // If the last button pressed was a calculation and a new digit is pressed, start a new calculation
+    console.log(lastPressedWasCalculation);
+    console.log(isDigit);
+    if (lastPressedWasCalculation && isDigit) {
+      setInput(value);
+      setLastPressedWasCalculation(false);
+    } else {
+      // Append the value to the input for all other cases
+      setInput(input + value);
+      setLastPressedWasCalculation(false);
     }
   };
 
@@ -52,6 +74,7 @@ function App() {
   const handleCalculate = () => {
     try {
       setInput(eval(input).toString());
+      console.log(input);
     } catch (error) {
       setInput("Error");
     }
@@ -65,7 +88,9 @@ function App() {
     <div className="App">
       <h1>Calculator</h1>
       <div className="calculator dark">
-        <div className="display">{input}</div>
+        <div className="display" data-testid="display">
+          {input}
+        </div>
         <div className="buttons">
           <button onClick={() => handleButtonClick("7")}>7</button>
           <button onClick={() => handleButtonClick("8")}>8</button>
@@ -80,12 +105,16 @@ function App() {
             -
           </button>
           <button onClick={() => handleButtonClick("1")}>1</button>
-          <button onClick={() => handleButtonClick("2")}>2</button>
+          <button onClick={() => handleButtonClick("2")} data-testid="number2">
+            2
+          </button>
           <button onClick={() => handleButtonClick("3")}>3</button>
           <button className="operator" onClick={() => handleButtonClick("*")}>
             *
           </button>
-          <button onClick={() => handleButtonClick("0")}>0</button>
+          <button onClick={() => handleButtonClick("0")} data-testid="number0">
+            0
+          </button>
           <button onClick={() => handleButtonClick(".")}>.</button>
           <button className="equal" onClick={handleCalculate}>
             =
@@ -101,19 +130,11 @@ function App() {
             x<sup>2</sup>
           </button>
           <button onClick={() => handleButtonClick("+-")}>+/-</button>
-          <button onClick={
-            ()=> handleButtonClick("sqrt")
-          }>
+          <button onClick={() => handleButtonClick("sqrt")}>
             sqrt<sup>x</sup>
           </button>
-          <button
-          onClick={() => handleButtonClick("1/x")}
-          >
-            1/x
-          </button>
-          <button onClick={()=>clearEntry()}>
-            CE
-          </button>
+          <button onClick={() => handleButtonClick("1/x")}>1/x</button>
+          <button onClick={() => clearEntry()}>CE</button>
         </div>
       </div>
     </div>
